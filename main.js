05194b71103b736bc4108865df451c1db07c8ffa
otabek05 <otabektoshmukhammedov@gmail.com>
2022-11-15 19:39:31 +0900

    request practices

--- main.js
+++ main.js
@@ -2,7 +2,22 @@ var http=require('http')
 const fs=require('fs')
 http.createServer(function(req,res){
     res.setHeader('Content-Type', 'text/html')
-    fs.readFile('./views/main.html', (err,data)=>{
+    let path= "./views/"
+    switch(req.url){
+        case "/":
+            path+='main.html'
+            break
+        case "/about":
+            path+="about.html"
+            break
+        case "/log":
+            path+="log.html"
+            break
+        default:
+            path+="404.html"
+
+    }
+    fs.readFile(path, (err,data)=>{
         if (err){
             console.log(err)
             res.end()
